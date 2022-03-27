634039aeade4156ce107d9f29b96d15d9af6e753
MARCO ANTONIO HERNANDEZ <mhernandezn1602@alumno.ipn.mx>
2022-03-27 16:30:22 -0600

    Main

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:servicesapp/splashScreen/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ServicesApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MySplashScreen(),
+    );
+  }
+}
