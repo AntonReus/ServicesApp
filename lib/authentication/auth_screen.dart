642c3d6cdcb0bdd79adb20ecd20314bc72b990ff
AntonReus <mhernandezn1602@alumno.ipn.mx>
2022-03-27 22:10:42 -0600

    v1

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -10,9 +10,67 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bienvenido"),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [
+                  Colors.cyan,
+                  Colors.amber,
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp,
+              )),
+            ),
+            title: const Text("iFood",
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                  fontFamily: "Lobster",
+                )),
+            centerTitle: true,
+            bottom: const TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(
+                    Icons.lock,
+                    color: Colors.white,
+                  ),
+                  text: "Login",
+                ),
+                Tab(
+                  icon: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  text: "Registrer",
+                )
+              ],
+              indicatorColor: Colors.white38,
+              indicatorWeight: 6,
+            )),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.amber,
+              Colors.cyan,
+            ],
+          )),
+          child: const TabBarView(
+            children: [
+              LoginScreen(),
+              SignUpScreen(),
+            ],
+          ),
+        ),
       ),
     );
   }
